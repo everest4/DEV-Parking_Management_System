8420f284974088b80fb3fa8127334766e1697e8e
Everest Kotaj <everest.kotaj44@gmail.com>
2025-11-19 01:31:17 +0100

    perfect

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {}
